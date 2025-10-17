e5a122f29e1efcd6c7afb345cd6801a39f2dd2f1
Zograb <mkrtch.zograb@gmail.com>
2025-10-17 13:39:13 +0400

    🔧 refactor dynamic import in NoteEditor component

--- apps/web/components/ui/NoteEditor.tsx
+++ apps/web/components/ui/NoteEditor.tsx
@@ -3,7 +3,6 @@
 import dynamic from 'next/dynamic'
 
 export const NoteEditor = dynamic(
-  () =>
-    import('@nexly/ui/Editor').then((mod) => ({ default: mod.Editor })),
+  () => import('@nexly/ui/Editor').then((mod) => ({ default: mod.Editor })),
   { ssr: false },
 )
