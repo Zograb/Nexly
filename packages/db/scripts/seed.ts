135d949a7d870dc9627d78070cf91694fd13d0e7
Zograb <mkrtch.zograb@gmail.com>
2025-10-17 14:10:44 +0400

    🌱 add @faker-js/faker for user data generation and update seed script to create random users

--- packages/db/scripts/seed.ts
+++ packages/db/scripts/seed.ts
@@ -1,16 +1,24 @@
+import { faker } from '@faker-js/faker'
 import { PrismaClient, Prisma } from '@prisma/client'
-
-const users: Prisma.UserCreateManyInput[] = [
-  { email: 'test@test.com', preferredName: 'Test' },
-]
 
 const prisma = new PrismaClient()
 
-const seedUsers = async () => {
+const seedUsers = async (amount = 5) => {
+  const users: Prisma.UserCreateManyInput[] = []
+
+  for (let i = 0; i < amount; i++) {
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    users.push({
+      email: faker.internet.email({ firstName, lastName }),
+      preferredName: `${firstName} ${lastName}`,
+    })
+  }
+
   await prisma.user.createMany({
     data: users,
   })
-  console.log('Users seeded successfully', users.length)
+  console.log(`Users seeded successfully: ${amount}`)
 }
 
 async function main() {
