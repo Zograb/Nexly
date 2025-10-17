fc4546b993eead6cf652d6bd84a0c1a889cd4c07
Zograb <mkrtch.zograb@gmail.com>
2025-10-17 11:24:36 +0400

    🔧 update User model and migration
    - Changed primary key of User model from Int to String with UUID default.
    - Added preferredName field to User model.
    - Updated migration script to reflect changes in User table structure, including new timestamps and constraints.

---
+++ packages/db/prisma/migrations/20251017072320_add_user_base_fields/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "deletedAt" TIMESTAMP(3),
+ADD COLUMN     "preferredName" TEXT,
+ADD COLUMN     "updatedAt" TIMESTAMP(3),
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
