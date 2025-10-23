fa94e3eb4ba86f8d549899872467053909673cbb
Zograb <mkrtch.zograb@gmail.com>
2025-10-23 22:42:19 +0400

    🎨 enhance Editor styles
    - add custom styles for paragrpah, headings, hr, lists, blockquotes, and code elements

--- packages/ui/src/components/Editor/Editor.css.ts
+++ packages/ui/src/components/Editor/Editor.css.ts
@@ -14,3 +14,62 @@ globalStyle(`${editorBase} .tiptap`, {
 globalStyle(`${editorBase} .ProseMirror-focused:focus-visible`, {
   outline: 'none',
 })
+
+globalStyle(`${editorBase} .tiptap::selection`, {
+  backgroundColor: 'var(--color-primary)',
+})
+
+globalStyle(`${editorBase} .tiptap p`, {
+  fontSize: 16,
+})
+
+globalStyle(`${editorBase} .tiptap h1`, {
+  fontSize: 40,
+})
+
+globalStyle(`${editorBase} .tiptap h2`, {
+  fontSize: 28,
+})
+
+globalStyle(`${editorBase} .tiptap ul`, {
+  listStyleType: 'disc',
+  paddingLeft: 25,
+})
+
+globalStyle(`${editorBase} .tiptap ul[data-type="taskList"]`, {
+  listStyleType: 'none',
+  paddingLeft: 25,
+})
+
+globalStyle(`${editorBase} .tiptap ul[data-type="taskList"] li`, {
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  gap: 12,
+})
+
+globalStyle(`${editorBase} .tiptap ol`, {
+  listStyleType: 'decimal',
+  paddingLeft: 25,
+})
+
+globalStyle(`${editorBase} .tiptap blockquote`, {
+  borderLeft: '4px solid var(--color-foreground-border)',
+  paddingLeft: 16,
+})
+
+globalStyle(`${editorBase} .tiptap pre`, {
+  background: 'var(--color-foreground)',
+  padding: '4px 8px',
+  borderRadius: 6,
+})
+
+globalStyle(`${editorBase} .tiptap code`, {
+  background: 'var(--color-foreground)',
+  padding: '4px 8px',
+  borderRadius: 6,
+})
+
+globalStyle(`${editorBase} .tiptap hr`, {
+  border: '1px solid var(--color-foreground-border)',
+})
