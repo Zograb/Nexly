4a506e7c9e8c42e4ee99e6a5f6857c6ece857718
Zograb <mkrtch.zograb@gmail.com>
2025-10-24 17:37:36 +0400

    ✨ add onChange, initialContent props to Editor component

--- packages/ui/src/components/Editor/Editor.tsx
+++ packages/ui/src/components/Editor/Editor.tsx
@@ -7,14 +7,17 @@ import { useEditor, EditorContent, EditorContext } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { useMemo } from 'react'
 
-import { Button } from '../Button'
-
 import { BubbleMenu } from './components/BubbleMenu/BubbleMenu'
 import { FloatingMenu } from './components/FloatingMenu'
 import { editorBase } from './Editor.css'
 import { SlashCommands } from './extensions/SlashCommands'
 
-export const Editor = () => {
+export interface EditorProps {
+  onChange: (html: string) => void
+  initialContent?: string
+}
+
+export const Editor = ({ onChange, initialContent }: EditorProps) => {
   const editor = useEditor({
     extensions: [
       TextStyle,
@@ -31,14 +34,16 @@ export const Editor = () => {
       }),
       SlashCommands,
     ],
-    content: '',
+    onUpdate: ({ editor }) => {
+      onChange?.(editor.getHTML())
+    },
+    content: initialContent ?? '',
   })
 
   const providerValue = useMemo(() => ({ editor }), [editor])
 
   return (
     <div className="max-w-[800px] h-full mx-auto">
-      <Button onClick={() => console.log(editor.getHTML())}>Get HTML</Button>
       <EditorContext.Provider value={providerValue}>
         <EditorContent className={editorBase} editor={editor} />
         <FloatingMenu editor={editor} />
