1d89fd771dbc072aa43f79201d4549d36d4c60fc
Zograb <mkrtch.zograb@gmail.com>
2025-11-03 14:51:12 +0400

    🗃️ update database schema
    - Removed unique constraints on the Folder model's name field and adjusted related indices in schema.zmodel.
    - Added a new migration script to drop existing unique constraints on Folder and Note models.

---
+++ packages/db/prisma/migrations/20251103051027_remove_unique_name_constraint_for_folder/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "public"."Folder_name_key";
+
+-- DropIndex
+DROP INDEX "public"."Folder_userId_name_key";
+
+-- DropIndex
+DROP INDEX "public"."Note_title_key";
+
+-- DropIndex
+DROP INDEX "public"."Note_userId_title_key";
