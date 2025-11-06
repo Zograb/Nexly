e462b555fe0ec17f0cac993bd2380a586e241d99
Zograb <mkrtch.zograb@gmail.com>
2025-11-06 17:16:55 +0400

    👔 add functionality to create new notes

--- apps/web/src/components/layout/Sidebar.tsx
+++ apps/web/src/components/layout/Sidebar.tsx
@@ -1,31 +1,130 @@
-import type { User } from 'src/hooks/useCurrentUser'
+import type {
+  CreateNoteMutation,
+  DeleteNoteMutation,
+} from 'src/graphql/generated/graphql'
 
+import { useMutation } from '@apollo/client/react'
 import { useAuth } from '@clerk/clerk-react'
 import { useNavigate } from '@tanstack/react-router'
 import clsx from 'clsx'
-import { FileText, House, LogOut, Search, Settings } from 'lucide-react'
+import { FileText, House, LogOut, Search, Settings, Trash } from 'lucide-react'
+import { useCallback } from 'react'
 
+import { Button } from '@nexly/ui/components/Button'
 import { Folder } from 'src/components/ui/Folder'
 import { NavLink } from 'src/components/ui/NavLink'
+import { graphql } from 'src/graphql/generated'
+import { useCurrentUser } from 'src/hooks/useCurrentUser'
 import { getUserInitials } from 'src/utils/helpers/getUserInitials'
 
-export interface SidebarProps {
-  user: User
-}
-
-export const Sidebar = ({ user }: SidebarProps) => {
+export const Sidebar = () => {
   const { signOut } = useAuth()
   const navigate = useNavigate()
+  const { user } = useCurrentUser()
+
+  const [createNote] = useMutation<CreateNoteMutation>(
+    graphql(`
+      mutation CreateNote($note: NoteCreateInput!) {
+        createNote(note: $note) {
+          id
+          title
+          content
+        }
+      }
+    `),
+  )
+  const [deleteNote] = useMutation<DeleteNoteMutation>(
+    graphql(`
+      mutation DeleteNote($noteId: String!) {
+        deleteNote(noteId: $noteId)
+      }
+    `),
+  )
 
   const folders = user?.folders || []
 
-  const logout = async () => {
+  const logout = useCallback(async () => {
     await signOut()
 
     navigate({
       to: '/login',
     })
-  }
+  }, [signOut, navigate])
+
+  const addNewNote = useCallback(
+    async (folderId: string) => {
+      const result = await createNote({
+        variables: {
+          note: {
+            title: 'New Note',
+            content: '',
+            folder: {
+              connect: {
+                id: folderId,
+              },
+            },
+            user: {
+              connect: {
+                id: user?.id,
+              },
+            },
+          },
+        },
+        update: (cache, { data }) => {
+          const newNote = data?.createNote
+
+          if (!newNote) return
+
+          // Get a reference to the newly created note in the cache
+          const newNoteRef = cache.identify({
+            __typename: 'Note',
+            id: newNote.id,
+          })
+
+          // Add the reference to the folder's notes array
+          cache.modify({
+            id: cache.identify({ __typename: 'Folder', id: folderId }),
+            fields: {
+              notes(existingNotes = []) {
+                return [...existingNotes, { __ref: newNoteRef }]
+              },
+            },
+          })
+        },
+      })
+
+      const note = result.data?.createNote
+
+      if (note?.id) {
+        navigate({
+          to: `/note/$noteId`,
+          params: { noteId: note.id },
+        })
+      }
+    },
+    [user?.id, navigate, createNote],
+  )
+
+  const handleDeleteNote = useCallback(
+    (noteId: string) => {
+      deleteNote({
+        variables: { noteId },
+        optimisticResponse: {
+          deleteNote: true,
+        },
+        update: (cache, { data }) => {
+          const deletedNote = data?.deleteNote
+          if (!deletedNote) return
+
+          cache.evict({
+            id: cache.identify({ __typename: 'Note', id: noteId }),
+          })
+          cache.gc()
+        },
+      })
+    },
+    [deleteNote],
+  )
 
   return (
     <div className="w-[260px] bg-foreground h-screen border-r border-foreground-border py-4 px-2">
@@ -53,18 +152,32 @@ export const Sidebar = ({ user }: SidebarProps) => {
           </div>
           <div className="flex flex-col gap-2 mt-14">
             {folders.map((folder) => (
-              <Folder key={folder.id} name={folder.name}>
+              <Folder
+                key={folder.id}
+                name={folder.name}
+                onAdd={() => addNewNote(folder.id)}
+              >
                 <div className="w-full flex flex-col gap-1 py-1">
                   {folder.notes?.map((note) => (
-                    <NavLink
-                      key={note.id}
-                      to={`/note/$noteId`}
-                      params={{ noteId: note.id }}
-                      icon={<FileText size={16} />}
-                      className="pl-4 pr-4 text-xs"
-                    >
-                      {note.title}
-                    </NavLink>
+                    <div key={note.id} className="relative group">
+                      <NavLink
+                        to={`/note/$noteId`}
+                        params={{ noteId: note.id }}
+                        icon={<FileText size={16} />}
+                        className="pl-4 pr-4 text-xs"
+                      >
+                        {note.title}
+                      </NavLink>
+                      <div className="absolute right-0 top-0 h-full p-1 hidden group-hover:flex">
+                        <Button
+                          variant="secondary"
+                          size="icon"
+                          onClick={() => handleDeleteNote(note.id)}
+                        >
+                          <Trash className="text-error" size={16} />
+                        </Button>
+                      </div>
+                    </div>
                   ))}
                 </div>
               </Folder>
--- apps/web/src/components/ui/Folder.tsx
+++ apps/web/src/components/ui/Folder.tsx
@@ -1,27 +1,59 @@
-import { FolderClosed, FolderOpen } from 'lucide-react'
-import { type PropsWithChildren, useState } from 'react'
+import { FolderClosed, FolderOpen, Plus } from 'lucide-react'
+import {
+  type MouseEvent,
+  type PropsWithChildren,
+  useCallback,
+  useState,
+} from 'react'
 
+import { Button } from '@nexly/ui/components/Button'
 import { Collapsible } from '@nexly/ui/components/Collapsible/Collapsible'
 import { CollapsibleContent } from '@nexly/ui/components/Collapsible/CollapsibleContent'
 import { CollapsibleTrigger } from '@nexly/ui/components/Collapsible/CollapsibleTrigger'
+import { cn } from '@nexly/ui/utils'
 
 import { NavLink } from './NavLink'
 
 export interface FolderProps extends PropsWithChildren {
+  onAdd?: () => void
   name: string
 }
 
-export const Folder = ({ name, children }: FolderProps) => {
+export const Folder = ({ name, children, onAdd }: FolderProps) => {
   const [isOpen, setIsOpen] = useState(false)
+
+  const handleAdd = useCallback(
+    (e: MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation()
+      onAdd?.()
+    },
+    [onAdd],
+  )
 
   return (
     <Collapsible className="w-full" open={isOpen} onOpenChange={setIsOpen}>
       <CollapsibleTrigger asChild>
-        <NavLink
-          icon={isOpen ? <FolderOpen size={16} /> : <FolderClosed size={16} />}
-        >
-          {name}
-        </NavLink>
+        <div className="relative group">
+          <NavLink
+            icon={
+              isOpen ? <FolderOpen size={16} /> : <FolderClosed size={16} />
+            }
+          >
+            <span className="text-ellipsis overflow-hidden whitespace-nowrap group-hover:pr-12">
+              {name}
+            </span>
+          </NavLink>
+          <div
+            className={cn(
+              'absolute h-full right-0 top-0 gap-0.5',
+              'hidden group-hover:flex items-center justify-center p-1',
+            )}
+          >
+            <Button variant="secondary" size="icon" onClick={handleAdd}>
+              <Plus size={16} />
+            </Button>
+          </div>
+        </div>
       </CollapsibleTrigger>
       <CollapsibleContent>{children}</CollapsibleContent>
     </Collapsible>
